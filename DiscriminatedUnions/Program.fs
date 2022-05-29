9039e95e9b0a05df594d85c0d04cd5e82a0b057d
Michal Moudrý <moudry.michal@live.com>
2022-05-29 14:34:15 +0200

    Finished second exercise on dis. unions

--- DiscriminatedUnions/Program.fs
+++ DiscriminatedUnions/Program.fs
@@ -31,3 +31,54 @@ let rateActivity (activity: Activity): Approval =
 
 let rating = rateActivity (Activity.Movie(Genre.Thriller))
 printfn $"{rating}"
+
+
+open System.Text
+
+type Plant =
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
+
+let getPlantType (plantChar: char) =
+    match plantChar with
+        | 'G' -> Some(Grass)
+        | 'C' -> Some(Clover)
+        | 'R' -> Some(Radishes)
+        | 'V' -> Some(Violets)
+        | _ -> None
+
+let plantStringToList (plantString: string) =
+    printfn $"Plant string: {plantString}"
+    [
+        getPlantType(plantString[0]).Value
+        getPlantType(plantString[1]).Value
+        getPlantType(plantString[2]).Value
+        getPlantType(plantString[3]).Value
+    ]
+
+let plants (diagram: string) (student: string) =
+    let str = new StringBuilder(4)
+    let columns = 
+        match student with
+            | "Alice" -> [0; 1]
+            | "Bob" -> [2; 3]
+            | "Charlie" -> [4; 5]
+            | "David" -> [6; 7]
+            | "Eve" -> [8; 9]
+            | "Fred" -> [10; 11]
+            | "Ginny" -> [12; 13]
+            | "Harriet" -> [14; 15]
+            | "Ileana" -> [16; 17]
+            | "Joseph" -> [18; 19]
+            | "Kincaid" -> [20; 21]
+            | "Larry" -> [22; 23]
+            | _ -> []
+    let rows = diagram.Split("\n")
+    columns |> List.iter (fun i -> str.Append(rows[0][i]) |> ignore)
+    columns |> List.iter (fun i -> str.Append(rows[1][i]) |> ignore)
+    plantStringToList(str.ToString())
+
+let plantsResult = plants "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV" "Eve"
+printfn $"{plantsResult}"
