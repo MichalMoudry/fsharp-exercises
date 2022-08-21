139ec030820a277c841bf600f04c73a27cc456ed
Michal Moudrý <moudry.michal@live.com>
2022-08-21 20:26:25 +0200

    Updates to robot simulator exercise

--- Exercises/RobotSimulator.fs
+++ Exercises/RobotSimulator.fs
@@ -3,22 +3,44 @@ namespace Exercises
 module RobotSimulator =
     type Direction = North | East | South | West
     type Position = int * int
-    type Robot = { direction: Direction; position: Position }
+    type Robot =
+        { direction: Direction; position: Position }
+        member this.updateDirection(dir: Direction) =
+            { direction = dir; position = this.position }
     type Movement = TurnRight | TurnLeft | Advance
 
-    let matchInstructions (instruction: char): Movement =
+    let create direction position =
+        { direction = direction; position = position }
+
+    let matchInstruction (instruction: char): Movement =
         match instruction with
             | 'R' -> TurnRight
             | 'L' -> TurnLeft
             | _ -> Advance
     
-    let changePosition (robot: Robot) (instruction: char) =
-        let movement = matchInstructions instruction
-        0
-
-    let create direction position =
-        { direction = direction; position = position }
+    let handleTurnRight (robot: Robot) (origDirection: Direction) =
+        let newDirection =
+            match origDirection with
+                | North -> East
+                | East -> South
+                | South -> West
+                | _ -> North
+        robot.updateDirection(newDirection)
+    
+    let handleTurnLeft (robot: Robot) (origDirection: Direction) =
+        let newDirection =
+            match origDirection with
+                | North -> West
+                | _ -> North
+        robot.updateDirection(newDirection)
+    
+    let executeInstructions (robot: Robot) (instruction: char) =
+        let movement = matchInstruction instruction
+        match movement with
+            | Advance -> robot
+            | TurnRight -> handleTurnRight robot robot.direction
+            | TurnLeft -> handleTurnLeft robot robot.direction
 
     let move (instructions: string) (robot: Robot) =
-        instructions |> Seq.iter (fun i -> printfn "%c %s" i ((matchInstructions i).ToString()))
-        robot
+        let robotHistory = instructions |> Seq.map (fun i -> executeInstructions robot i)
+        robotHistory |> Seq.last
