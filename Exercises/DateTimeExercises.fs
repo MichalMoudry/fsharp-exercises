272d327127e2be2872fa5e214b416e9ca64e6d70
Michal Moudrý <moudry.michal@live.com>
2022-08-24 19:35:27 +0200

    Added correct day number to meetup exercise

--- Exercises/DateTimeExercises.fs
+++ Exercises/DateTimeExercises.fs
@@ -47,17 +47,12 @@ module DateTimeExercises =
     let meetup year month week (dayOfWeek: DayOfWeek): DateTime =
         let weekNumber = getWeekNumber week
         if weekNumber.IsSome then
-            (*let dayNumber = getDayNumber dayOfWeek
-            let mutable dateWithMonths = DateTime().AddYears(year - 1).AddMonths(month - 1).AddDays(float(7 * weekNumber.Value))
-            let initDayNumber = getDayNumber dateWithMonths.DayOfWeek
-            dateWithMonths <- dateWithMonths.AddDays(-initDayNumber.Value);
-            printfn $"Day of the week: {dateWithMonths.DayOfWeek} | {dayNumber.Value}"*)
             let mutable initDate = DateTime().AddYears(year - 1).AddMonths(month - 1)
             let initDayNumber = (getDayNumber initDate.DayOfWeek).Value
-            initDate <- initDate.AddDays(-initDayNumber)
+            let inputDayNumber = (getDayNumber dayOfWeek).Value
+            initDate <- initDate.AddDays(-initDayNumber).AddDays(inputDayNumber) // Set to monday + add input days
             let isMonday = initDate.DayOfWeek = DayOfWeek.Monday
-            //let isFirstWeekOverlap = not(isMonday) // && dayOfWeek < initDate.DayOfWeek
-            printfn $"Is monday: {isMonday} | {initDate.DayOfWeek} | {not(isMonday)}"
+            //printfn $"Is monday: {isMonday} | {initDate.DayOfWeek}"
             initDate
         else
             DateTime.Now
